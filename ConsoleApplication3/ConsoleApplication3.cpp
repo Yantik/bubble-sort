aca79e7870d2bb0f29c4a5e58cd11d9c0af1fa35
Yantik <Foxmen12345678910@mail.ru>
2022-11-16 19:35:04 +0300

    Добавьте файлы проекта.

---
+++ ConsoleApplication3/ConsoleApplication3.cpp
@@ -0,0 +1,33 @@
+﻿#include <iostream>
+#include <ctime>
+using namespace std;
+int main()
+{
+	srand(time(0));
+	int temp = 0;
+	// кол-во элементов
+	const int s =10;
+	int arr[s];
+	cout << "unsorted array" << endl;
+	for (int i = 0; i < s; i++) arr[i] = rand() % 1000 + 1;
+	for (int i = 0; i < s; i++) cout << arr[i] << "\t";
+	for (int i = 0; i < s; i++)
+	{
+		for (int i = 0; i < (s-1); i++)
+		{
+			if (arr[i] > arr[i + 1])
+			{
+				if ((i + 1) != s)
+				{
+					 temp = arr[i];
+					arr[i] = arr[i + 1];
+					arr[i + 1] = temp;
+				}
+			}
+		}
+	}
+	cout << "\nsorted array" << endl;
+	for (int i = 0; i < s; i++) {cout << arr[i] << "\t";}
+}
+
+
